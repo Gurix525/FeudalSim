67d90e8b4104076e7590ddf7c232ecc410a655f1
Gurix525 <gurix525@gmail.com>
2023-03-09 04:11:24 +0100

    Uv generating only one time

--- Assets/TerrainChunkActivator.cs
+++ Assets/TerrainChunkActivator.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 
 public class TerrainChunkActivator : MonoBehaviour
 {
+    private float _timeSinceLastActivation = 0F;
+
     private void FixedUpdate()
     {
-        ActivateChunk();
+        _timeSinceLastActivation += Time.fixedDeltaTime;
+        if (_timeSinceLastActivation > 0.5F)
+            ActivateChunk();
     }
 
     private void ActivateChunk()
     {
-        Vector2Int currentChunkPos = new(((int)Math.Floor(transform.position.x)) / 100, ((int)Math.Floor(transform.position.z)) / 100);
-        TerrainGenerator.ChangeActiveChunk(currentChunkPos);
+        Vector2Int currentChunkPos = new((int)Mathf.Floor(transform.position.x / 100F), (int)Mathf.Floor(transform.position.z / 100F));
+        if (TerrainGenerator.Instance.ActiveChunk.Position != currentChunkPos)
+        {
+            _timeSinceLastActivation = 0F;
+            TerrainGenerator.Instance.ActiveChunk = TerrainGenerator.Instance.Chunks[currentChunkPos];
+        }
     }
 }
--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Profiling;
 
@@ -15,6 +17,11 @@ public class TerrainGenerator : MonoBehaviour
     private MeshCollider _meshCollider;
     private Dictionary<Vector2Int, Chunk> _chunks = new();
     private Chunk _activeChunk;
+    private Mesh _mesh;
+    private int[] _triangles = new int[532824];
+    private Vector3[] _vertices = new Vector3[90000];
+    private int _meshInstanceId;
+    private bool _isBaking = false;
 
     public Chunk ActiveChunk
     {
@@ -26,27 +33,39 @@ public class TerrainGenerator : MonoBehaviour
         }
     }
 
-    public static void ChangeActiveChunk(Vector2Int chunkPos)
-    {
-        if (Instance.ActiveChunk.Position != chunkPos)
-            Instance.ActiveChunk = Instance._chunks[chunkPos];
-    }
+    public Dictionary<Vector2Int, Chunk> Chunks
+        => _chunks;
 
     private void Awake()
     {
+        Instance = this;
         _meshFilter = GetComponent<MeshFilter>();
         _meshCollider = GetComponent<MeshCollider>();
-        Instance = this;
+        InitializeMesh();
         UpdateTerrain();
+    }
+
+    private void InitializeMesh()
+    {
+        _mesh = new();
+        _mesh.MarkDynamic();
+        _meshFilter.mesh = _mesh;
+        _mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        _meshInstanceId = _mesh.GetInstanceID();
+    }
+
+    private IEnumerable<Vector2> GetUvs()
+    {
+        for (int z = -100; z < 200; z++)
+            for (int x = -100; x < 200; x++)
+                yield return new Vector2(x, z);
     }
 
     private void UpdateTerrain()
     {
         Profiler.BeginSample("UpdateTerrain");
         GenerateChunks();
-        Mesh mesh = GenerateMesh();
-        _meshFilter.mesh = mesh;
-        _meshCollider.sharedMesh = mesh;
+        GenerateMesh();
         Profiler.EndSample();
     }
 
@@ -69,13 +88,12 @@ public class TerrainGenerator : MonoBehaviour
         Profiler.EndSample();
     }
 
-    private Mesh GenerateMesh()
+    private void GenerateMesh()
     {
         Profiler.BeginSample("GenerateMesh");
-        List<Vector3> vertices = new();
-        List<int> triangles = new();
         int size = 299;
 
+        int index = 0;
         Profiler.BeginSample("VertexAssigning");
         for (int z = 0; z < size + 1; z++)
             for (int x = 0; x < size + 1; x++)
@@ -97,38 +115,81 @@ public class TerrainGenerator : MonoBehaviour
                 else
                     chunkX = _activeChunk.X + 1;
 
-                vertices.Add(new(chunkX * 100 + x % 100, _chunks[new(chunkX, chunkZ)][x % 100, z % 100], chunkZ * 100 + z % 100));
+                _vertices[index] = new(chunkX * 100 + x % 100, _chunks[new(chunkX, chunkZ)][x % 100, z % 100], chunkZ * 100 + z % 100);
+                index++;
             }
         Profiler.EndSample();
         Profiler.BeginSample("CreatingTriangles");
+        index = 0;
         for (int z = 0; z < size - 1; z++)
             for (int x = 0; x < size - 1; x++)
             {
                 int i = (z * size) + z + x;
 
-                triangles.Add(i);
-                triangles.Add(i + size + 1);
-                triangles.Add(i + size + 2);
-                triangles.Add(i);
-                triangles.Add(i + size + 2);
-                triangles.Add(i + 1);
+                _triangles[index] = i;
+                _triangles[index + 1] = i + size + 1;
+                _triangles[index + 2] = i + size + 2;
+                _triangles[index + 3] = i;
+                _triangles[index + 4] = i + size + 2;
+                _triangles[index + 5] = i + 1;
+                index += 6;
             }
         Profiler.EndSample();
         Profiler.BeginSample("CreatingMesh");
-        Mesh mesh = new();
-        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
+        _mesh.Clear();
+        _mesh.vertices = _vertices;
+        _mesh.triangles = _triangles;
         Profiler.EndSample();
         Profiler.BeginSample("Creating UV");
-        mesh.uv = vertices
-            .Select(x => new Vector2(x.x, x.z))
-            .ToArray();
-        mesh.RecalculateNormals();
-        mesh.RecalculateTangents();
+        if (_mesh.uv.Length == 0)
+        {
+            List<Vector2> uvs = new();
+            foreach (var uv in GetUvs())
+                uvs.Add(uv);
+            _mesh.uv = uvs.ToArray();
+        }
+        _mesh.RecalculateNormals();
+        _mesh.RecalculateTangents();
         Profiler.EndSample();
+        StartCoroutine(AssignMeshToColliderCoroutine());
         Profiler.EndSample();
-        return mesh;
+    }
+
+    private Task BakePhysicsMesh()
+    {
+        Debug.Log("Task started");
+        try
+        {
+            Physics.BakeMesh(_meshInstanceId, false);
+            Debug.Log("MeshBaked");
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+        Debug.Log("Task finished");
+        return Task.CompletedTask;
+    }
+
+    private IEnumerator AssignMeshToColliderCoroutine()
+    {
+        if (_isBaking)
+            yield break;
+        _isBaking = true;
+        var task = Task.Run(BakePhysicsMesh);
+        while (true)
+        {
+            if (task.IsCompleted)
+                break;
+            if (Input.GetKeyDown(KeyCode.U))
+                break;
+            yield return null;
+        }
+        Debug.Log("Finished coroutine");
+        Profiler.BeginSample("Assigning mesh to collider");
+        _meshCollider.sharedMesh = _mesh;
+        _isBaking = false;
+        Profiler.EndSample();
     }
 }
 
