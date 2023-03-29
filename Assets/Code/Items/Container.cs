a6e049b2a1042dd7b2977e34aa7823694e13124e
Gurix525 <57004218+Gurix525@users.noreply.github.com>
2023-03-29 21:11:25 +0200

    Added Item.cs and Container.cs

---
+++ Assets/Code/Items/Container.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Items
+{
+    public class Container
+    {
+        public List<Item> Items { get; } = new();
+    }
+}
---
+++ Assets/Code/Items/Item.cs
@@ -0,0 +1,7 @@
+namespace Items
+{
+    public class Item
+    {
+
+    }
+}
