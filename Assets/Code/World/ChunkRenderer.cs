5a99b1a46eaf67314c898047edba6f6e848bb9c1
Gurix525 <gurix525@gmail.com>
2023-04-21 02:38:20 +0200

    Added ChunkRenderer.Boulders

--- Assets/Code/World/ChunkRenderer.cs
+++ Assets/Code/World/ChunkRenderer.cs
@@ -16,6 +16,7 @@ namespace World
         public Vector2Int Position { get; private set; }
         public Transform Buildings { get; private set; }
         public Transform ItemHandlers { get; private set; }
+        public Transform Boulders { get; private set; }
 
         private MeshFilter _meshFilter;
         private MeshCollider _meshCollider;
@@ -44,6 +45,8 @@ namespace World
             Buildings.transform.parent = transform;
             ItemHandlers = new GameObject("ItemHandlers").transform;
             ItemHandlers.transform.parent = transform;
+            Boulders = new GameObject("Boulders").transform;
+            Boulders.transform.parent = transform;
         }
 
         private void InitializeMesh()
--- Assets/Code/World/Terrain.cs
+++ Assets/Code/World/Terrain.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using Controls;
-using UnityEngine.UIElements;
 
 namespace World
 {
