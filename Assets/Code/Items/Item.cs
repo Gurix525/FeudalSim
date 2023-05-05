1b9603b7c4cc8b3d1102cd0ef01fa52d1f835482
Gurix525 <gurix525@gmail.com>
2023-05-05 02:18:59 +0200

    Added wood item

--- Assets/Code/Items/Item.cs
+++ Assets/Code/Items/Item.cs
@@ -19,6 +19,7 @@ namespace Items
         {
             { "Stone", new("Stone", actions: new ItemAction[] {new BuildAction()})},
             { "Wood", new("Wood", 20, actions: new ItemAction[] {new BuildAction()}) },
+            { "Planks", new("Planks", 20, actions: new ItemAction[] {new BuildAction()}) },
             { "Sword", new("Sword", 1, stats: new(){ { "ArmorType", "Head" }, { "InventorySlots", "3"} }) },
             { "Axe", new("Axe", 1, actions: new ItemAction[] {new AxeAction(),  new DestroyAction()}) },
             { "Shovel", new("Shovel", 1, actions: new ItemAction[] {new ShovelAction()}) },
