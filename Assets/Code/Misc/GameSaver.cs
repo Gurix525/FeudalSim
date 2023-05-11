57422a360738f1bcac00bb92102e16a2e6bf9630
Gurix525 <gurix525@gmail.com>
2023-05-11 19:22:55 +0200

    Saving world and player info

--- Assets/Code/Misc/GameSaver.cs
+++ Assets/Code/Misc/GameSaver.cs
@@ -7,12 +7,21 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class GameSaver : MonoBehaviour
 {
+    private string _allSavesPath;
+    private string _savePath = string.Empty;
+
+    private void Awake()
+    {
+        _allSavesPath = Path.Combine(Application.persistentDataPath, "Saves");
+    }
+
     private void OnEnable()
     {
         PlayerController.MainSave.AddListener(ActionType.Started, SaveGame);
     }
 
     private void OnDisable()
+
     {
         PlayerController.MainSave.RemoveListener(ActionType.Started, SaveGame);
     }
@@ -21,10 +30,27 @@ public class GameSaver : MonoBehaviour
 
     private void SaveGame(CallbackContext context)
     {
-        string allSavesPath = Path.Combine(Application.persistentDataPath, "Saves");
-        Directory.CreateDirectory(allSavesPath);
-        string savePath = Path.Combine(allSavesPath, NoiseSampler.Seed.ToString());
-        Directory.CreateDirectory(savePath);
+        Directory.CreateDirectory(_allSavesPath);
+        _savePath = Path.Combine(_allSavesPath, "A");
+        Directory.CreateDirectory(_savePath);
+        SaveWorldInfo();
+        SavePlayerInfo();
+    }
+
+    private void SaveWorldInfo()
+    {
+        string worldInfoPath = Path.Combine(_savePath, "World.txt");
+        string dataToWrite = "A";
+        dataToWrite += $"\n{NoiseSampler.Seed}";
+        File.WriteAllText(worldInfoPath, dataToWrite);
+    }
+
+    private void SavePlayerInfo()
+    {
+        string playerInfoPath = Path.Combine(_savePath, "Player.txt");
+        Saves.PlayerInfo playerInfo = new();
+        string json = JsonUtility.ToJson(playerInfo);
+        File.WriteAllText(playerInfoPath, json);
     }
 
     #endregion Private
---
+++ Assets/Code/Saves/PlayerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Misc;
+using UnityEngine;
+
+namespace Saves
+{
+    [Serializable]
+    public class PlayerInfo
+    {
+        public Vector2 Position;
+
+        public PlayerInfo()
+        {
+            Position = References.GetReference("Player").transform.position;
+        }
+    }
+}
