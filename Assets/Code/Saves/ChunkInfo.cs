94e2fcdce7d39d5cb2a0c8bdc4dd529fb7774d6f
Gurix525 <gurix525@gmail.com>
2023-05-12 02:25:37 +0200

    Removed CellInfo.cs

--- Assets/Code/Saves/ChunkInfo.cs
+++ Assets/Code/Saves/ChunkInfo.cs
@@ -9,24 +9,31 @@ namespace Saves
     public class ChunkInfo
     {
         public Vector2Int Position;
-        public CellInfo[] Cells;
         public int[] Heights;
+        public Color[] Colors;
+        public int[][] FloorHeights;
+        public int[][] HorizontalWallHeights;
+        public int[][] VerticalWallHeights;
 
         public ChunkInfo(Chunk chunk)
         {
-            CellInfo[] cellInfos = new CellInfo[10000];
-            int[] heights = new int[10000];
             Position = chunk.Position;
+            Heights = new int[10000];
+            Colors = new Color[10000];
+            FloorHeights = new int[10000][];
+            HorizontalWallHeights = new int[10000][];
+            VerticalWallHeights = new int[10000][];
+
             Cell[] cells = chunk.Cells.Values.ToArray();
             for (int i = 0; i < 10000; i++)
             {
                 Cell cell = cells[i];
-                cellInfos[i] = new(cell);
-                heights[i] = cell.Height;
+                Heights[i] = cell.Height;
+                Colors[i] = cell.Color;
+                FloorHeights[i] = cell.FloorHeights.ToArray();
+                HorizontalWallHeights[i] = cell.HorizontalWallHeights.ToArray();
+                VerticalWallHeights[i] = cell.VerticalWallHeights.ToArray();
             }
-
-            Cells = cellInfos;
-            Heights = heights;
         }
     }
 }
