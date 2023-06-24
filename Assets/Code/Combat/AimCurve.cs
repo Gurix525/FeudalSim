895e25e19e91bc5ae28a08d8522082d60ad24bd4
Gurix525 <gurix525@gmail.com>
2023-06-24 12:49:00 +0200

    Bezier works

--- Assets/Code/Combat/AimCurve.cs
+++ Assets/Code/Combat/AimCurve.cs
@@ -29,10 +29,10 @@ namespace Combat
             if (_nodesCount < 2)
                 return;
             Vector3[] nodes = new Vector3[_nodesCount];
-            for (int i = 1; i < _nodesCount + 1; i++)
+            for (int i = 0; i < _nodesCount; i++)
             {
-                float t = 1 / (float)_nodesCount;
-                nodes[i - 1] = GetBezierPoint(t);
+                float t = (float)i / (_nodesCount - 1);
+                nodes[i] = GetBezierPoint(t);
             }
             _renderer.positionCount = nodes.Length;
             _renderer.SetPositions(nodes);
@@ -43,13 +43,9 @@ namespace Combat
             Vector3 p0 = P1.transform.position;
             Vector3 p1 = P2.transform.position;
             Vector3 p2 = P3.transform.position;
-            float u = 1 - t;
-            float tt = t * t;
-            float uu = u * u;
-            Vector3 p = uu * p0;
-            p += 2 * u * t * p1;
-            p += tt * p2;
-            return p;
+            return Mathf.Pow(1 - t, 2) * p0
+                + 2 * (1 - t) * t * p1
+                + t * t * p2;
         }
     }
 }
