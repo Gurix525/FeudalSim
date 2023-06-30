77f52f09abcd7697008dad53a1cdc1f64f26941d
Gurix525 <gurix525@gmail.com>
2023-06-30 15:34:29 +0200

    Update Stats.cs

--- Assets/Code/PlayerControls/Stats.cs
+++ Assets/Code/PlayerControls/Stats.cs
@@ -2,14 +2,10 @@ using System;
 using System.Collections.Generic;
 using PlayerControls;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Stats : MonoBehaviour
 {
-    [field: SerializeField] public float CurrentHP { get; private set; }
-    [field: SerializeField] public float MaxHP { get; private set; }
-    [field: SerializeField] public float CurrentStamina { get; private set; }
-    [field: SerializeField] public float MaxStamina { get; private set; }
-
     private Dictionary<string, Skill> _skills = new()
     {
         { "Running", Skill.Zero },
@@ -20,4 +16,27 @@ public class Stats : MonoBehaviour
         { "Parrying", Skill.Zero },
         { "Evading", Skill.Zero }
     };
+
+    public UnityEvent<string, Skill> SkillLevelIncreased { get; } = new();
+
+    [field: SerializeField] public float CurrentHP { get; private set; }
+    [field: SerializeField] public float MaxHP { get; private set; }
+    [field: SerializeField] public float CurrentStamina { get; private set; }
+    [field: SerializeField] public float MaxStamina { get; private set; }
+
+    public Skill GetSkill(string skill)
+    {
+        return _skills[skill];
+    }
+
+    public void IncreaseSkill(string skill, float xp)
+    {
+        if (xp <= 0F)
+            throw new ArgumentException("Dodawany xp musi być dodatni.");
+        int previousLevel = _skills[skill].Level;
+        _skills[skill] += xp;
+        int currentLevel = _skills[skill].Level;
+        if (currentLevel > previousLevel)
+            SkillLevelIncreased.Invoke(skill, _skills[skill]);
+    }
 }
