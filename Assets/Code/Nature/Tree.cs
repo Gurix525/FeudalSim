ed31df9272e6eda79551a780578a9041448b8173
Gurix525 <gurix525@gmail.com>
2023-07-06 01:00:21 +0200

    Disabled log spawning on tree destroy

--- Assets/Code/Nature/Tree.cs
+++ Assets/Code/Nature/Tree.cs
@@ -38,15 +38,15 @@ namespace Nature
             Equipment.Insert(item);
             if (item.Count > 0)
                 item.Drop(Player.Position);
-            GameObject log = Instantiate(
-                Resources.Load<GameObject>("Prefabs/Nature/Log"),
-                transform.parent.parent.parent);
-            log.transform.position = transform.position;
-            log.transform.localScale = transform.parent.localScale;
-            log.GetComponent<TemporaryRigidbody>().ActivateRigidbody(
-                Vector3.forward * 100F,
-                log.transform.position + Vector3.up * 10F,
-                200F);
+            //GameObject log = Instantiate(
+            //    Resources.Load<GameObject>("Prefabs/Nature/Log"),
+            //    transform.parent.parent.parent);
+            //log.transform.position = transform.position;
+            //log.transform.localScale = transform.parent.localScale;
+            //log.GetComponent<TemporaryRigidbody>().ActivateRigidbody(
+            //    Vector3.forward * 100F,
+            //    log.transform.position + Vector3.up * 10F,
+            //    200F);
             Destroy(transform.parent.gameObject);
             TerrainRenderer.MarkNavMeshToReload();
             Player.Instance.Stats.AddSkill("Woodcutting", 1F);
