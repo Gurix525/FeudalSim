a310981a25a24acfe8d45e61ec3462819411c744
Gurix525 <gurix525@gmail.com>
2023-09-03 18:50:45 +0200

    Added bloom and dof options

---
+++ Assets/Code/UI/MainMenu/Option.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public abstract class Option : MonoBehaviour
+    {
+        protected Settings _settings;
+
+        private Toggle _toggle;
+        private Slider _slider;
+        private Dropdown _dropdown;
+
+        public void Initialize(Settings settings)
+        {
+            _settings = settings;
+            _toggle = GetComponentInChildren<Toggle>();
+            _slider = GetComponentInChildren<Slider>();
+            _dropdown = GetComponentInChildren<Dropdown>();
+
+            if (_toggle != null)
+                _toggle.onValueChanged.AddListener(ExecuteToggle);
+            if (_slider != null)
+                _slider.onValueChanged.AddListener(ExecuteSlider);
+            if (_dropdown != null)
+                _dropdown.onValueChanged.AddListener(ExecuteDropdown);
+        }
+
+        public void Execute()
+        {
+            if (_toggle != null)
+                ExecuteToggle(_toggle.isOn);
+            if (_slider != null)
+                ExecuteSlider(_slider.value);
+            if (_dropdown != null)
+                ExecuteDropdown(_dropdown.value);
+        }
+
+        protected virtual void ExecuteToggle(bool arg)
+        { }
+
+        protected virtual void ExecuteSlider(float arg)
+        { }
+
+        protected virtual void ExecuteDropdown(int arg)
+        { }
+    }
+}
--- Assets/Code/UI/MainMenu/Settings.cs
+++ Assets/Code/UI/MainMenu/Settings.cs
@@ -1,8 +1,30 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace UI
 {
     public class Settings : MonoBehaviour
     {
+        [field: SerializeField] public VolumeProfile Profile { get; set; }
+
+        private Option[] _options;
+
+        private void Awake()
+        {
+            _options = GetComponentsInChildren<Option>();
+            InitializeAllOptions();
+        }
+
+        private void InitializeAllOptions()
+        {
+            foreach (var option in _options)
+                option.Initialize(this);
+        }
+
+        private void ExecuteAllOptions()
+        {
+            foreach (var option in _options)
+                option.Execute();
+        }
     }
 }
