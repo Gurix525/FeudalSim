f444626504e2fa67cb9b096e54292c56be57a966
Gurix525 <gurix525@gmail.com>
2023-10-14 18:17:03 +0200

    Fixed Exit to menu button

--- Assets/Code/UI/PauseMenu/ExitToMenuButton.cs
+++ Assets/Code/UI/PauseMenu/ExitToMenuButton.cs
@@ -19,6 +19,8 @@ namespace UI
             new TaskManager.Task(Exit());
         }
 
+        private void OnLeftMouseButton(InputValue value) { }
+
         private IEnumerator Exit()
         {
             LoadingScreen.Enable();
