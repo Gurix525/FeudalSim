f30aab4ad0aa16df1e191950932d0610aed7f040
Gurix525 <gurix525@gmail.com>
2023-12-16 04:18:37 +0100

    Add door

---
+++ Assets/Code/Buildings/Door.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Controls;
+using UnityEngine;
+
+namespace Buildings
+{
+    public class Door : MonoBehaviour, IMouseHandler
+    {
+        private float _originalRotation = 0F;
+        private float _targetRotation = 0F;
+        private bool _isInitialized = false;
+
+        public void OnLeftMouseButton(Vector2 position)
+        {
+            if (!_isInitialized)
+            {
+                _originalRotation = transform.rotation.y;
+                _isInitialized = true;
+            }
+            _targetRotation = Math.Abs(_targetRotation - 90F);
+            transform.rotation = Quaternion.Euler(0F, _originalRotation + _targetRotation, 0F);
+        }
+    }
+}
